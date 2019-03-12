d0b299817bcaa6ce86bca22e31f78bc1c4f95dae
xbingo2 <2532551433@qq.com>
2019-03-12 18:00:42 +0800

    Add files via upload

---
+++ trackDemo/pages/index/index.js
@@ -0,0 +1,57 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    background: ['demo-text-1', 'demo-text-2', 'demo-text-3'],
+    indicatorDots: true,
+    vertical: false,
+    autoplay: true,
+    circular: false,
+    interval: 2000,
+    duration: 500,
+    previousMargin: 0,
+    nextMargin: 0,
+    code: '',
+  },
+  changeProperty: function (e) {
+    var propertyName = e.currentTarget.dataset.propertyName
+    var newData = {}
+    newData[propertyName] = e.detail.value
+    this.setData(newData)
+  },
+  changeIndicatorDots: function (e) {
+    this.setData({
+      indicatorDots: !this.data.indicatorDots
+    })
+  },
+  changeAutoplay: function (e) {
+    this.setData({
+      autoplay: !this.data.autoplay
+    })
+  },
+  intervalChange: function (e) {
+    this.setData({
+      interval: e.detail.value
+    })
+  },
+  durationChange: function (e) {
+    this.setData({
+      duration: e.detail.value
+    })
+  },
+
+  scanCode() {
+    const that = this
+    wx.scanCode({
+      success(res) {
+        that.setData({
+          code: res.result
+        })
+      },
+      fail() { }
+    })
+  }
+})
+
